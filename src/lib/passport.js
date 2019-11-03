155c4083ee6a3fb9a0ffc57b3be9f7c83e17e66d
Harold Velez <hvelez56@gmail.com>
2019-11-03 00:27:50 -0500

    add sign up functionality

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,15 +1,31 @@
 const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
+const pool = require('../database');
+const helper = require('./helpers');
 
 passport.use('local.signup', new localStrategy({
     usernameField: 'username',
     passwordField: 'password',
-    fullnameField: 'fullname',
+    //fullnameField: 'fullname',
     passReqToCallback: true
-}, async (req,username,password,fullname,done)=>{
-    console.log(req.body);
+}, async (req,username,password,done)=>{
+    const {fullname} = req.body;
+    const newUser = {
+        username,
+        password,
+        fullname
+    };
+    newUser.password = await helper.encrypPassword(password);
+    const result = await pool.query("INSERT INTO  users SET ?",[newUser]);
+    newUser.id = result.insertId;
+    return done(null, newUser);
 }));
 
 passport.serializeUser((usr, done)=>{
-    
-})
+    done(null,usr.id)
+});
+
+passport.deserializeUser(async (id, done)=>{
+    const row = await pool.query("SELECT * FROM users where id = ? ",[id]);
+    done(null, row[0]);
+});
